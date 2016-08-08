fce5f84ecf1e7c9c32b295a006c7f3cb1bd4478d
paulossm <paulosergios11@hotmail.com>
2016-08-08 11:15:00 -0300

    first commit

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,32 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="en">
+
+	<!-- bootstrap CSS framework -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page" col-xs-12 col-lg-10 col-lg-offset-1>
+    <?php echo $content; ?>
+</div><!-- page -->
+<footer class="col-xs-12">
+    <p class="text-center col-lg-4 col-lg-offset-4">Multiprova<br>ECT - UFRN<br>2016</p>
+</footer>
+    <!-- BOOTSTRAP JS -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ protected/views/site/index.php
@@ -0,0 +1,56 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<section id="headInfo" class="col-xs-12">
+    <div class="col-xs-6 text-left">
+        <h4>Avaliação I</h4>
+        <h5>Álgebra Linear (CET00430)</h5>
+    </div>
+    
+    <div class="col-xs-6 text-right">
+        <h4>Questão <span id="n_questao">1</span>/<span id="t_questao">9</span></h4>
+    </div>
+</section>
+
+<section id="prova" class="col-xs-12">
+    <div id="enunciado" class="jumbotron">
+        <h1>Enunciado</h1>
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    </div>
+    
+    <div id="resposta">
+        <div class="panel panel-default">
+            <div class="panel-body">
+              Alternativa
+            </div>
+        </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-body">
+              Alternativa
+            </div>
+        </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-body">
+              Alternativa
+            </div>
+        </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-body">
+              Alternativa
+            </div>
+        </div>
+        
+        <div class="panel panel-default">
+            <div class="panel-body">
+              Alternativa
+            </div>
+        </div>
+    </div>
+</section>
+
